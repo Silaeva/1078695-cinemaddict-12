3e2712ed36b7e6ca1dd35266e3f7a6f1348034f4
Silaeva <tkachenko481@mail.ru>
2020-09-28 19:06:46 +0300

    убирает вывод в консоль

--- src/main.js
+++ src/main.js
@@ -53,11 +53,8 @@ filmListPresenter.init();
 
 window.addEventListener(`load`, () => {
   navigator.serviceWorker.register(`/sw.js`)
-    .then(() => {
-      console.log(`ServiceWorker available`); // eslint-disable-line
-    }).catch(() => {
-      console.error(`ServiceWorker isn't available`); // eslint-disable-line
-    });
+    .then(() => {})
+    .catch(() => {});
 });
 
 window.addEventListener(`online`, () => {
